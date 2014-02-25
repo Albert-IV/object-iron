1f24b6a9bdb24da11ee54ee5416010c8dbfe3cbc
Albert Engelbrecht <dropped.on.japan@gmail.com>
2014-02-25 14:43:23 -0600

    Improved tests.

--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
+var assert = require('assert');
 var flatten = require('../index.js');
+
 var nestedObject = {
   name : {
     legal : "Sam Fisher",
     given : {
       first : "Samantha",
       last : "Fisherson"
-    }
+    },
+    isCool : false
   },
   info : {
     needless : {
@@ -18,27 +21,96 @@ var nestedObject = {
     tags : [
       "person",
       "tagged",
-      "arrays are cool"
+      { "complex" : {
+        "stuff" : [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+      } }
     ]
   }
 };
 
-var flatObject = flatten(nestedObject);
-var shouldBe = '{\n\
+// Test default flattening
+var fullFlatObject = flatten(nestedObject);
+var fullShouldBe = '{\n\
+  "name.legal": "Sam Fisher",\n\
+  "name.given.first": "Samantha",\n\
+  "name.given.last": "Fisherson",\n\
+  "name.isCool": false,\n\
+  "info.needless.nesting.check.it": "out",\n\
+  "info.tags[0]": "person",\n\
+  "info.tags[1]": "tagged",\n\
+  "info.tags[2].complex.stuff[0]": 1,\n\
+  "info.tags[2].complex.stuff[1]": 2,\n\
+  "info.tags[2].complex.stuff[2]": 3,\n\
+  "info.tags[2].complex.stuff[3]": 4,\n\
+  "info.tags[2].complex.stuff[4]": 5,\n\
+  "info.tags[2].complex.stuff[5]": 6,\n\
+  "info.tags[2].complex.stuff[6]": 7,\n\
+  "info.tags[2].complex.stuff[7]": 8\n\
+}';
+
+assert.equal( JSON.stringify(fullFlatObject, null, '  '), fullShouldBe, "Non-filtered objects failed!" );
+
+// Test single filtering support
+var stringFilteredObject = flatten(nestedObject, ['string']);
+var stringFilteredShouldBe = '{\n\
+  "name.isCool": false,\n\
+  "info.tags[2].complex.stuff[0]": 1,\n\
+  "info.tags[2].complex.stuff[1]": 2,\n\
+  "info.tags[2].complex.stuff[2]": 3,\n\
+  "info.tags[2].complex.stuff[3]": 4,\n\
+  "info.tags[2].complex.stuff[4]": 5,\n\
+  "info.tags[2].complex.stuff[5]": 6,\n\
+  "info.tags[2].complex.stuff[6]": 7,\n\
+  "info.tags[2].complex.stuff[7]": 8\n\
+}';
+
+assert.equal( JSON.stringify(stringFilteredObject, null, '  '), stringFilteredShouldBe, "String-filtered objects failed!" );
+
+// Test single filtering support
+var numFilteredObject = flatten(nestedObject, ['number']);
+var numFilteredShouldBe = '{\n\
+  "name.legal": "Sam Fisher",\n\
+  "name.given.first": "Samantha",\n\
+  "name.given.last": "Fisherson",\n\
+  "name.isCool": false,\n\
+  "info.needless.nesting.check.it": "out",\n\
+  "info.tags[0]": "person",\n\
+  "info.tags[1]": "tagged"\n\
+}';
+
+assert.equal( JSON.stringify(numFilteredObject, null, '  '), numFilteredShouldBe, "Number-filtered objects failed!" );
+
+// Test single filtering support
+var boolFilteredObject = flatten(nestedObject, ['boolean']);
+var boolFilteredShouldBe = '{\n\
   "name.legal": "Sam Fisher",\n\
   "name.given.first": "Samantha",\n\
   "name.given.last": "Fisherson",\n\
   "info.needless.nesting.check.it": "out",\n\
   "info.tags[0]": "person",\n\
   "info.tags[1]": "tagged",\n\
-  "info.tags[2]": "arrays are cool"\n\
+  "info.tags[2].complex.stuff[0]": 1,\n\
+  "info.tags[2].complex.stuff[1]": 2,\n\
+  "info.tags[2].complex.stuff[2]": 3,\n\
+  "info.tags[2].complex.stuff[3]": 4,\n\
+  "info.tags[2].complex.stuff[4]": 5,\n\
+  "info.tags[2].complex.stuff[5]": 6,\n\
+  "info.tags[2].complex.stuff[6]": 7,\n\
+  "info.tags[2].complex.stuff[7]": 8\n\
 }';
 
-if( JSON.stringify(flatObject, null, '  ') !== shouldBe ) {
-  console.log(JSON.stringify(flatObject, null, '  '));
-  console.log(shouldBe);
-  throw Error('Values did not come out as expected!');
-} else {
-  console.log("Test passed!");
-  process.exit(0);
-}
+assert.equal( JSON.stringify(boolFilteredObject, null, '  '), boolFilteredShouldBe, "Boolean-filtered objects failed!" );
+
+// Test single filtering support
+var arrFilteredObject = flatten(nestedObject, ['array']);
+var arrFilteredShouldBe = '{\n\
+  "name.legal": "Sam Fisher",\n\
+  "name.given.first": "Samantha",\n\
+  "name.given.last": "Fisherson",\n\
+  "name.isCool": false,\n\
+  "info.needless.nesting.check.it": "out"\n\
+}';
+
+assert.equal( JSON.stringify(arrFilteredObject, null, '  '), arrFilteredShouldBe, "Array-filtered objects failed!" );
+
+console.log("Tests Passed!");
